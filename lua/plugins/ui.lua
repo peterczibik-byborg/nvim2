0e746d5913456b9258c93fd64ffad0da20a96628
Peter Czibik <peter.czibik@byborgenterprises.com>
2024-12-20 14:22:58 +0100

    Finish up some coding plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,16 +1,15 @@
 return {
   {
     "ellisonleao/gruvbox.nvim",
-    config = function() 
-      vim.cmd.colorscheme"gruvbox" 
+    config = function()
+      vim.cmd.colorscheme "gruvbox"
     end,
   },
   {
-    "echasnovski/mini.nvim",
-    config = function()
-      local statusline = require'mini.statusline'
-      statusline.setup{use_icons=true}
-    end,
+    "echasnovski/mini.statusline",
+    opts = {
+      use_icons = true,
+    },
   },
   {
     "nvim-telescope/telescope.nvim",
@@ -18,17 +17,17 @@ return {
       {
         "<C-p>",
         -- FIXME: these requires are going to be running before initial install
-        require"telescope.builtin".find_files,
+        require "telescope.builtin".find_files,
         desc = "Find files",
       },
       {
         "<leader>fs",
-        require"telescope.builtin".live_grep,
+        require "telescope.builtin".live_grep,
         desc = "Search in project",
       },
       {
         "<leader>fu",
-        require"telescope.builtin".lsp_references,
+        require "telescope.builtin".lsp_references,
         desc = "Find LSP References",
       },
     },
